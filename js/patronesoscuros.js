6b27c364e67666fbd17248e7decce5ca035e7a0e
LGCatalina <catalina.lopez.g@mail.pucv.cl>
2024-05-16 15:22:15 -0400

    ola

--- js/patronesoscuros.js
+++ js/patronesoscuros.js
@@ -1,9 +1,5 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Agrega un evento clic al título "MANIPULACIÓN"
-    document.getElementById('titulo-manipulacion').addEventListener('click', function() {
-        // Obtiene el contenido asociado
-        var contenido = document.getElementById('contenido-manipulacion');
-        // Alterna la clase 'oculto' para mostrar u ocultar el contenido
-        contenido.classList.toggle('oculto');
-    });
-});
+function toggleContent() {
+    var contenido = document.getElementById("contenido");
+    contenido.classList.toggle("mostrar");
+  }
+  
