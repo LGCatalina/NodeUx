00c3d46e37d14dcb0e7859830bbc8e606a094bdb
LGCatalina <catalina.lopez.g@mail.pucv.cl>
2024-05-22 00:56:02 -0400

    cambios

--- scripts.js
+++ scripts.js
@@ -1,7 +1,3 @@
-// Función para mostrar el mensaje al intentar salir de la página
-window.onbeforeunload = function() {
-    return "¿Estás seguro de que quieres abandonar esta página?";
-};
 
 function scrollToTarget(targetId) {
     document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
